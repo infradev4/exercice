b15d5c801f2a5693742edbbc167b32c9f9646125
infradev4 <82362516+infradev4@users.noreply.github.com>
2021-04-29 23:33:12 +0200

    Finalisation de l'exercice

---
+++ Exercice 1.8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercice_1._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double anneeConvertie;
+            double calculA;
+            double calculB;
+            double MilleNeufCents = 1900;
+
+
+            Console.WriteLine("Programme d'identification d'année bissextile");
+            Console.WriteLine("Entrée une année au format XXXX :");
+            string annee = Console.ReadLine();
+
+            bool anneeTest = double.TryParse(annee, out anneeConvertie);
+
+            if (anneeTest == false)
+            {
+                Console.WriteLine("Erreur de saisie");
+            }
+
+            calculA = (anneeConvertie % 4);
+            calculB = (anneeConvertie % 100);
+
+            if (calculA % 4 == 0 & calculB % 100 != 0 || anneeConvertie == MilleNeufCents)
+            {
+                Console.WriteLine("Année bissextile");
+            }
+            else
+            {
+                Console.WriteLine("Année non-bissextile");
+            }
+                            Console.ReadLine();
+        }
+    }
+}
