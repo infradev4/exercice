cae7a50bf7066fc2605b3172f464b7680ab600b5
CRM <crm@arfp.asso.fr>
2021-04-30 09:50:35 +0200

    finalisation de l'exercice

---
+++ Exercice 1.9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercice_1._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diviseur = 2;
+            int total;
+            int entrerConvertie;
+
+            Console.WriteLine("Recherche des diviseurs d’un nombre");
+            Console.WriteLine("Entrer un nombre entier :");
+
+            string entrer=Console.ReadLine();
+
+            bool entrerTester=int.TryParse(entrer,out entrerConvertie);
+
+            if (entrerTester)
+            {
+                for (diviseur = 2; diviseur < entrerConvertie; diviseur++)
+                {
+                    total = (entrerConvertie % diviseur);
+                    Console.WriteLine("diviseur " + diviseur + " " + "total " + total);
+                    //Console.WriteLine(total);
+
+                if (total == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
