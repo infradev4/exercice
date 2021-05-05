cde79a0dac91ab061cfc999482b9e5491e89a2d7
infradev4 <82362516+infradev4@users.noreply.github.com>
2021-05-06 01:42:32 +0200

    en cours de finalisation

--- Exercice 1.10C/Program.cs
+++ Exercice 1.10C/Program.cs
@@ -9,35 +9,34 @@ namespace Exercice_1._10C
             string a;
             int compt = 0; 
             int saisie;
-            int k;
-            int somdiv;
-            int nbr;
+            int diviseur;
+            int sommeDiviseurs;
+            int nombre = 2;
 
             Console.WriteLine("Programme de recherche des nombres parfaits");
             Console.WriteLine("Combien de nombre parfaits souhaitez-vous afficher ? :");
 
             a = Console.ReadLine();
             saisie = int.Parse(a);
-            nbr = 2;
             
             while (compt != saisie)
             {
-                somdiv = 1;
-                k = 2;
+                sommeDiviseurs = 1;
+                diviseur = 2;
                 
-                    while(k <= nbr/2 )
+                    while(diviseur <= nombre/2 )
                     {
-                        if (nbr % k == 0) 
-                        somdiv += k;
-                        k++;
+                        if (nombre % diviseur == 0) 
+                        sommeDiviseurs += diviseur;
+                        diviseur++;
                     }
                     
-                    if (somdiv == nbr)
+                    if (sommeDiviseurs == nombre)
                     {
-                        Console.WriteLine(nbr+" est un nombre parfait");
+                        Console.WriteLine(nombre+" est un nombre parfait");
                         compt++;
                     }
-                    nbr++;
+                    nombre++;
             }
         }
     }
