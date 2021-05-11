a91d735c8b4276a33e99a55c84e7ef006f31a7f3
infradev4 <82362516+infradev4@users.noreply.github.com>
2021-05-11 13:33:40 +0200

    WinForms

---
+++ ADDITIONNEUR/ADDITIONNEUR/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace ADDITIONNEUR
+{
+    partial class Additionneur
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVider = new System.Windows.Forms.Button();
+            this.btnCalculer = new System.Windows.Forms.Button();
+            this.txbEcran = new System.Windows.Forms.TextBox();
+            this.btxChiffreZero = new System.Windows.Forms.Button();
+            this.btxChiffreUn = new System.Windows.Forms.Button();
+            this.btxChiffreDeux = new System.Windows.Forms.Button();
+            this.btxChiffreQuattre = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btxChiffreTrois = new System.Windows.Forms.Button();
+            this.btxChiffreNeuf = new System.Windows.Forms.Button();
+            this.btxChiffreCinq = new System.Windows.Forms.Button();
+            this.btxChiffreHuit = new System.Windows.Forms.Button();
+            this.btxChiffreSept = new System.Windows.Forms.Button();
+            this.btxChiffreSix = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVider
+            // 
+            this.btnVider.Location = new System.Drawing.Point(12, 332);
+            this.btnVider.Name = "btnVider";
+            this.btnVider.Size = new System.Drawing.Size(94, 29);
+            this.btnVider.TabIndex = 0;
+            this.btnVider.Text = "Vider";
+            this.btnVider.UseVisualStyleBackColor = true;
+            this.btnVider.Click += new System.EventHandler(this.btnVider_Click);
+            // 
+            // btnCalculer
+            // 
+            this.btnCalculer.Location = new System.Drawing.Point(202, 332);
+            this.btnCalculer.Name = "btnCalculer";
+            this.btnCalculer.Size = new System.Drawing.Size(94, 29);
+            this.btnCalculer.TabIndex = 1;
+            this.btnCalculer.Text = "Calculer";
+            this.btnCalculer.UseVisualStyleBackColor = true;
+            this.btnCalculer.Click += new System.EventHandler(this.btnCalculer_Click);
+            // 
+            // txbEcran
+            // 
+            this.txbEcran.Location = new System.Drawing.Point(12, 12);
+            this.txbEcran.Multiline = true;
+            this.txbEcran.Name = "txbEcran";
+            this.txbEcran.ReadOnly = true;
+            this.txbEcran.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbEcran.Size = new System.Drawing.Size(284, 115);
+            this.txbEcran.TabIndex = 2;
+            // 
+            // btxChiffreZero
+            // 
+            this.btxChiffreZero.Location = new System.Drawing.Point(13, 149);
+            this.btxChiffreZero.Name = "btxChiffreZero";
+            this.btxChiffreZero.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreZero.TabIndex = 3;
+            this.btxChiffreZero.Text = "0";
+            this.btxChiffreZero.UseVisualStyleBackColor = true;
+            this.btxChiffreZero.Click += new System.EventHandler(this.btxChiffreZero_Click);
+            // 
+            // btxChiffreUn
+            // 
+            this.btxChiffreUn.Location = new System.Drawing.Point(69, 149);
+            this.btxChiffreUn.Name = "btxChiffreUn";
+            this.btxChiffreUn.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreUn.TabIndex = 4;
+            this.btxChiffreUn.Text = "1";
+            this.btxChiffreUn.UseVisualStyleBackColor = true;
+            this.btxChiffreUn.Click += new System.EventHandler(this.btxChiffreUn_Click);
+            // 
+            // btxChiffreDeux
+            // 
+            this.btxChiffreDeux.Location = new System.Drawing.Point(125, 149);
+            this.btxChiffreDeux.Name = "btxChiffreDeux";
+            this.btxChiffreDeux.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreDeux.TabIndex = 5;
+            this.btxChiffreDeux.Text = "2";
+            this.btxChiffreDeux.UseVisualStyleBackColor = true;
+            this.btxChiffreDeux.Click += new System.EventHandler(this.btxChiffreDeux_Click);
+            // 
+            // btxChiffreQuattre
+            // 
+            this.btxChiffreQuattre.Location = new System.Drawing.Point(237, 149);
+            this.btxChiffreQuattre.Name = "btxChiffreQuattre";
+            this.btxChiffreQuattre.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreQuattre.TabIndex = 6;
+            this.btxChiffreQuattre.Text = "4";
+            this.btxChiffreQuattre.UseVisualStyleBackColor = true;
+            this.btxChiffreQuattre.Click += new System.EventHandler(this.btxChiffreQuattre_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(476, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btxChiffreTrois
+            // 
+            this.btxChiffreTrois.Location = new System.Drawing.Point(181, 149);
+            this.btxChiffreTrois.Name = "btxChiffreTrois";
+            this.btxChiffreTrois.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreTrois.TabIndex = 8;
+            this.btxChiffreTrois.Text = "3";
+            this.btxChiffreTrois.UseVisualStyleBackColor = true;
+            this.btxChiffreTrois.Click += new System.EventHandler(this.btxChiffreTrois_Click);
+            // 
+            // btxChiffreNeuf
+            // 
+            this.btxChiffreNeuf.Location = new System.Drawing.Point(237, 184);
+            this.btxChiffreNeuf.Name = "btxChiffreNeuf";
+            this.btxChiffreNeuf.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreNeuf.TabIndex = 13;
+            this.btxChiffreNeuf.Text = "9";
+            this.btxChiffreNeuf.UseVisualStyleBackColor = true;
+            this.btxChiffreNeuf.Click += new System.EventHandler(this.btxChiffreNeuf_Click);
+            // 
+            // btxChiffreCinq
+            // 
+            this.btxChiffreCinq.Location = new System.Drawing.Point(13, 184);
+            this.btxChiffreCinq.Name = "btxChiffreCinq";
+            this.btxChiffreCinq.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreCinq.TabIndex = 12;
+            this.btxChiffreCinq.Text = "5";
+            this.btxChiffreCinq.UseVisualStyleBackColor = true;
+            this.btxChiffreCinq.Click += new System.EventHandler(this.btxChiffreCinq_Click);
+            // 
+            // btxChiffreHuit
+            // 
+            this.btxChiffreHuit.Location = new System.Drawing.Point(181, 184);
+            this.btxChiffreHuit.Name = "btxChiffreHuit";
+            this.btxChiffreHuit.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreHuit.TabIndex = 11;
+            this.btxChiffreHuit.Text = "8";
+            this.btxChiffreHuit.UseVisualStyleBackColor = true;
+            this.btxChiffreHuit.Click += new System.EventHandler(this.btxChiffreHuit_Click);
+            // 
+            // btxChiffreSept
+            // 
+            this.btxChiffreSept.Location = new System.Drawing.Point(125, 184);
+            this.btxChiffreSept.Name = "btxChiffreSept";
+            this.btxChiffreSept.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreSept.TabIndex = 10;
+            this.btxChiffreSept.Text = "7";
+            this.btxChiffreSept.UseVisualStyleBackColor = true;
+            this.btxChiffreSept.Click += new System.EventHandler(this.btxChiffreSept_Click);
+            // 
+            // btxChiffreSix
+            // 
+            this.btxChiffreSix.Location = new System.Drawing.Point(69, 184);
+            this.btxChiffreSix.Name = "btxChiffreSix";
+            this.btxChiffreSix.Size = new System.Drawing.Size(50, 29);
+            this.btxChiffreSix.TabIndex = 9;
+            this.btxChiffreSix.Text = "6";
+            this.btxChiffreSix.UseVisualStyleBackColor = true;
+            this.btxChiffreSix.Click += new System.EventHandler(this.btxChiffreSix_Click);
+            // 
+            // Additionneur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 380);
+            this.Controls.Add(this.btxChiffreNeuf);
+            this.Controls.Add(this.btxChiffreCinq);
+            this.Controls.Add(this.btxChiffreHuit);
+            this.Controls.Add(this.btxChiffreSept);
+            this.Controls.Add(this.btxChiffreSix);
+            this.Controls.Add(this.btxChiffreTrois);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btxChiffreQuattre);
+            this.Controls.Add(this.btxChiffreDeux);
+            this.Controls.Add(this.btxChiffreUn);
+            this.Controls.Add(this.btxChiffreZero);
+            this.Controls.Add(this.txbEcran);
+            this.Controls.Add(this.btnCalculer);
+            this.Controls.Add(this.btnVider);
+            this.Name = "Additionneur";
+            this.Text = "Additionneur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVider;
+        private System.Windows.Forms.Button btnCalculer;
+        private System.Windows.Forms.TextBox txbEcran;
+        private System.Windows.Forms.Button btxChiffreZero;
+        private System.Windows.Forms.Button btxChiffreUn;
+        private System.Windows.Forms.Button btxChiffreDeux;
+        private System.Windows.Forms.Button btxChiffreQuattre;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btxChiffreTrois;
+        private System.Windows.Forms.Button btxChiffreNeuf;
+        private System.Windows.Forms.Button btxChiffreCinq;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btxChiffreSept;
+        private System.Windows.Forms.Button btxChiffreSix;
+        private System.Windows.Forms.Button btxChiffreHuit;
+    }
+}
+
---
+++ ADDITIONNEUR/ADDITIONNEUR/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADDITIONNEUR
+{
+    public partial class Additionneur : Form
+    {
+        public Additionneur()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculer_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnVider_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btxChiffreZero_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btxChiffreUn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btxChiffreDeux_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btxChiffreTrois_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btxChiffreQuattre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btxChiffreCinq_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btxChiffreSix_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btxChiffreSept_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btxChiffreHuit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btxChiffreNeuf_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
