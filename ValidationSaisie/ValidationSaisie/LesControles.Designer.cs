e737a45ecdbc9778dddedaef365d4b125060d684
infradev4 <82362516+infradev4@users.noreply.github.com>
2021-05-27 01:02:52 +0200

    suite

--- ValidationSaisie/ValidationSaisie/LesControles.Designer.cs
+++ ValidationSaisie/ValidationSaisie/LesControles.Designer.cs
@@ -29,13 +29,26 @@ namespace ValidationSaisie
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.LblNom = new System.Windows.Forms.Label();
             this.LblDate = new System.Windows.Forms.Label();
             this.LblMontant = new System.Windows.Forms.Label();
             this.LblFormat = new System.Windows.Forms.Label();
             this.LblCodePostal = new System.Windows.Forms.Label();
-            this.TxbNom = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txbDate = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txbMontant = new System.Windows.Forms.TextBox();
+            this.txbCodepostal = new System.Windows.Forms.TextBox();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.btnEffacer = new System.Windows.Forms.Button();
+            this.txbNom = new System.Windows.Forms.TextBox();
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider4 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).BeginInit();
             this.SuspendLayout();
             // 
             // LblNom
@@ -68,7 +81,7 @@ namespace ValidationSaisie
             // LblFormat
             // 
             this.LblFormat.AutoSize = true;
-            this.LblFormat.Location = new System.Drawing.Point(283, 94);
+            this.LblFormat.Location = new System.Drawing.Point(331, 97);
             this.LblFormat.Name = "LblFormat";
             this.LblFormat.Size = new System.Drawing.Size(106, 20);
             this.LblFormat.TabIndex = 3;
@@ -83,28 +96,81 @@ namespace ValidationSaisie
             this.LblCodePostal.TabIndex = 4;
             this.LblCodePostal.Text = "Code Postal";
             // 
-            // TxbNom
+            // txbDate
             // 
-            this.TxbNom.Location = new System.Drawing.Point(137, 50);
-            this.TxbNom.MaxLength = 30;
-            this.TxbNom.Name = "TxbNom";
-            this.TxbNom.Size = new System.Drawing.Size(125, 27);
-            this.TxbNom.TabIndex = 5;
+            this.txbDate.Location = new System.Drawing.Point(154, 94);
+            this.txbDate.Name = "txbDate";
+            this.txbDate.Size = new System.Drawing.Size(125, 27);
+            this.txbDate.TabIndex = 6;
             // 
-            // textBox1
+            // errorProvider1
             // 
-            this.textBox1.Location = new System.Drawing.Point(137, 94);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
-            this.textBox1.TabIndex = 6;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // txbMontant
+            // 
+            this.txbMontant.Location = new System.Drawing.Point(154, 136);
+            this.txbMontant.Name = "txbMontant";
+            this.txbMontant.Size = new System.Drawing.Size(125, 27);
+            this.txbMontant.TabIndex = 7;
+            // 
+            // txbCodepostal
+            // 
+            this.txbCodepostal.Location = new System.Drawing.Point(154, 177);
+            this.txbCodepostal.Name = "txbCodepostal";
+            this.txbCodepostal.Size = new System.Drawing.Size(125, 27);
+            this.txbCodepostal.TabIndex = 8;
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(185, 266);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(94, 29);
+            this.btnValider.TabIndex = 9;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
+            // 
+            // btnEffacer
+            // 
+            this.btnEffacer.Location = new System.Drawing.Point(185, 330);
+            this.btnEffacer.Name = "btnEffacer";
+            this.btnEffacer.Size = new System.Drawing.Size(94, 29);
+            this.btnEffacer.TabIndex = 10;
+            this.btnEffacer.Text = "Effacer";
+            this.btnEffacer.UseVisualStyleBackColor = true;
+            this.btnEffacer.Click += new System.EventHandler(this.btnEffacer_Click);
+            // 
+            // txbNom
+            // 
+            this.txbNom.Location = new System.Drawing.Point(154, 53);
+            this.txbNom.Name = "txbNom";
+            this.txbNom.Size = new System.Drawing.Size(125, 27);
+            this.txbNom.TabIndex = 11;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // errorProvider4
+            // 
+            this.errorProvider4.ContainerControl = this;
             // 
             // LesControles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.TxbNom);
+            this.Controls.Add(this.txbNom);
+            this.Controls.Add(this.btnEffacer);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.txbCodepostal);
+            this.Controls.Add(this.txbMontant);
+            this.Controls.Add(this.txbDate);
             this.Controls.Add(this.LblCodePostal);
             this.Controls.Add(this.LblFormat);
             this.Controls.Add(this.LblMontant);
@@ -112,6 +178,10 @@ namespace ValidationSaisie
             this.Controls.Add(this.LblNom);
             this.Name = "LesControles";
             this.Text = "LesControles";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,8 +194,17 @@ namespace ValidationSaisie
         private System.Windows.Forms.Label LblMontant;
         private System.Windows.Forms.Label LblFormat;
         private System.Windows.Forms.Label LblCodePostal;
-        private System.Windows.Forms.TextBox TxbNom;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txbDate;
+        private System.Windows.Forms.TextBox textDate;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox txbCodepostal;
+        private System.Windows.Forms.TextBox txbMontant;
+        private System.Windows.Forms.Button btnEffacer;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.TextBox txbNom;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+        private System.Windows.Forms.ErrorProvider errorProvider4;
     }
 }
 
--- ValidationSaisie/ValidationSaisie/LesControles.cs
+++ ValidationSaisie/ValidationSaisie/LesControles.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,5 +18,53 @@ namespace ValidationSaisie
             InitializeComponent();
         }
 
+        private void btnValider_Click(object sender, EventArgs e)
+        {
+            Regex regex = new Regex("^[AZa-z]{1,30}$");
+            Regex regexB = new Regex("^[0-9][0-9]+/[0-9][0-9]+/[0-9][0-9][0-9][0-9]+$");
+
+            if (txbNom.Text == "" & txbDate.Text == "" & txbMontant.Text == "" & txbCodepostal.Text == "")
+            {
+                errorProvider1.SetError(txbNom, "veuillez remplir tous les champs");
+                errorProvider2.SetError(txbDate, "veuillez remplir tous les champs");
+                errorProvider3.SetError(txbMontant, "veuillez remplir tous les champs");
+                errorProvider4.SetError(txbCodepostal, "veuillez remplir tous les champs");
+            }
+            else if ((!regex.Match(txbNom.Text).Success) & (!regexB.Match(txbDate.Text).Success))
+            {
+                errorProvider1.SetError(txbNom, "veuillez utiliser des caractères alphabétiques (max 30)");
+                errorProvider2.SetError(txbDate, "veuillez utiliser une date valide");
+            }
+            else
+            {
+                MessageBox.Show
+                    (
+                    "Nom : " + txbNom.Text + Environment.NewLine + 
+                    "Date : " + txbDate.Text + Environment.NewLine + 
+                    "Montant : " + txbMontant.Text + Environment.NewLine + 
+                    "Code Postal : " + txbCodepostal.Text, 
+                    "Validation Effectuée", MessageBoxButtons.OK
+                    );
+
+                DialogResult dialogresult = MessageBox.Show
+                    (
+                    "Fin de l’application ?", "FIN",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button1
+                    );
+                
+                if (dialogresult == DialogResult.Yes)
+                Application.Exit();
+            }
+        }
+
+        private void btnEffacer_Click(object sender, EventArgs e)
+        {
+            txbNom.Clear();
+            txbDate.Clear();
+            txbMontant.Clear();
+            txbCodepostal.Clear();
+        }
     }
 }
