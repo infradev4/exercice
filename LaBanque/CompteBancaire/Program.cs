93d1f18da91c70759d6595a42312de0c14d1362a
infradev4 <82362516+infradev4@users.noreply.github.com>
2021-06-15 00:27:21 +0200

    15_6_21_00h27

---
+++ LaBanque/CompteBancaire/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace CompteBancaire
+{
+    //définition d'une classe commence par le mot-clé "class"
+    public class CompteBancaire
+    {
+        //la définition des champs (attributs) de la classe CompteBancaire
+        //public string titulaire;
+        private string titulaire;
+
+        //public double solde;
+        private double solde;
+
+        //public string devise;
+        private string devise;
+
+        //Les accesseurs est une méthode permetent de modifier les attributs privés
+        public string Titulaire
+        {
+            get {return titulaire;}
+        //string titulaireCompte = compteChat.Titulaire;    // en lecture (getter)
+        //Ici, j'affiche la valeur de "titulaire" avec "titulaireCompte"
+           private set {titulaire = value;}    
+        }
+        
+        public double Solde
+        {
+            get {return solde;}
+           private set {solde = value;}
+        }
+        public string Devise
+        {
+            get {return devise;}
+           private set {devise = value;}
+        }
+
+        //la définition des méthodes de la classe
+        //les méthodes utilisent (et modifient) les valeurs des attributs
+        public void Crediter(double montant)
+        {
+            solde = solde + montant;
+            //solde += montant;
+        }
+        public void Debiter (double montant)
+        {
+            solde = solde - montant;
+            //solde -= montant;
+        }
+        //description d'un compte bancaire dans la class afin de ne pas avoir à le faire pour chaque objet
+        // Renvoie la description d'un compte
+        public string Decrire ()
+        {
+            string description = "Le solde du compte de " + titulaire + " est de " + solde + " " + devise;
+            return description;
+        }
+        //le constructeur me peremt d'instancier la class CompteBancaire avec des valeurs par defaut 
+        //afin de ne pas etre oblige de complete a chaque instanciation des informations repetitives.
+        public CompteBancaire(double soldeInitial, string leTitulaire, string laDevise)
+        {
+            solde = soldeInitial;
+            titulaire = leTitulaire;
+            devise = laDevise;
+        }
+    }
+    
+    /*
+    Ce code utilise la classe CompteBancaire définie plus haut pour créer le compte de xxx et y effectuer 
+    des opérations.
+    */
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+        /*
+        Déclaration et instanciation d'un nouvel objet sur une meme ligne
+        CompteBancaire compteOussama = new CompteBancaire();
+            ou
+        déclaration d'un nouvel objet Oussama
+        CompteBancaire compteOussama;
+        instanciation de l'objet Oussama
+        compteOussama = new CompteBancaire();
+        */
+        
+        /*
+        // affectations de valeurs aux attributs de la class CompteBancaire
+        compteOussama.titulaire = "Oussama";
+        compteOussama.solde = 0;
+        compteOussama.devise = "euro";
+
+        // appels des méthodes de la class dans notre objet
+        compteOussama.Crediter(850);
+        compteOussama.Debiter(100);
+
+        //déclaration d'un nouvel objet Amelyne sur une ligne
+        CompteBancaire compteAmelyne = new CompteBancaire();
+
+        compteAmelyne.titulaire = "Amelyne";
+        compteAmelyne.solde = 125;
+        compteAmelyne.devise = "euro";
+
+        Console.WriteLine("Entrez le montant de votre transfert : ");
+        
+        double montantTransfert;
+        montantTransfert = Convert.ToDouble(Console.ReadLine());
+        
+        compteOussama.Debiter(montantTransfert);
+        compteAmelyne.Crediter(montantTransfert);
+
+        Console.WriteLine(compteOussama.Decrire());
+        Console.WriteLine(compteAmelyne.Decrire());
+        
+        Remplacer par la methode "Decrire"
+        string description = "Le solde du compte de " + compteOussama.titulaire + 
+                            " est de " + compteOussama.solde + 
+                            " " + compteOussama.devise;
+        
+        Console.WriteLine(compteOussama.Decrire());
+        */
+
+        // déclaration et instanciation d'un nouvel objet en utilisant son constructeur
+        CompteBancaire compteChat = new CompteBancaire(0,"Chat","euro");
+
+        // appels de méthodes avec le constructeur
+
+        string titulaireCompte = compteChat.Titulaire;    // en lecture (getter)
+        //compteChat.Titulaire = "Galere";    // en écriture (setter)
+        //compteChat.Solde = 9;               // en écriture (setter)
+        //compteChat.Devise = "MAD";          // en écriture (setter)
+
+        compteChat.Crediter(150);
+        compteChat.Debiter(25);
+        Console.WriteLine(compteChat.Decrire());
+        }
+    }
+}
