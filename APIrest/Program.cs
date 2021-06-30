4cb87710b6bbff2b008deaacf2c19deefb3686fc
infradev4 <82362516+infradev4@users.noreply.github.com>
2021-06-30 19:15:42 +0200

    api

---
+++ APIrest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace APIrest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://weatherbit-v1-mashape.p.rapidapi.com/forecast/minutely?lat=47.9108&lon=7.2072&units=metric");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com");
+            IRestResponse response = client.Execute(request);
+        }
+    }
+}
